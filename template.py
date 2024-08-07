6d27fb35f7521a40fdfce7fcbca97e9d9266c140
vaibhavagrawal39 <vaibhavagrawal39@gmail.com>
2024-08-07 12:01:16 +0530

    requirements added

--- template.py
+++ template.py
@@ -19,7 +19,8 @@ list_of_files=[
     "requirements.txt",
     "setup.py",
     "research/trials.ipynb",
-    "test.py"
+    "templates/index.html"
+    
 ]
 for filepath in list_of_files:
     filepath = Path(filepath)
@@ -27,7 +28,6 @@ for filepath in list_of_files:
     if filedir !="":
         os.makedirs(filedir,exist_ok=True)
         logging.info(f"Creating directory: {filedir} for the file : {filename}")
-
 
     if (not os.path.exists(filepath)) or (os.path.getsize(filepath)==0):
         with open(filepath,"w") as f:
